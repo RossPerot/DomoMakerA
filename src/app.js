74f8422aa1bbc5b026c4ae0eefaec0b944e304fd
RossPerot <rmh8980@g.rit.edu>
2016-03-17 15:44:30 -0400

    test fix

--- src/app.js
+++ src/app.js
@@ -19,8 +19,10 @@ var router = require('./router.js');
 
 var port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+process.env.PWD = process.cwd();
+
 var app = express();
-app.use('/assets', express.static(path.resolve(__dirname+'../../client/')));
+app.use("/assets", express.static(path.resolve(process.env.PWD+'../../client/')));
 app.use(compression());
 app.use(bodyParser.urlencoded({
 	extended: true
